34c123012dd8b0948f5fac0644fd3b29d825593b
Albert <72774907+AlbertSimaoS@users.noreply.github.com>
2025-12-23 15:13:50 -0300

    Update and rename ErroSenha.java to SenhaInvalidaException.java

--- Exercicios/Exc 6/ErroSenha.java
+++
@@ -1,5 +0,0 @@
-class TamanhoDaSenhaException extends Exception {
-    public TamanhoDaSenhaException() {
-        super("A senha deve ter pelo menos 8 caracteres!"); // mensagem já definida
-    }
-}
